a36c764aa416714476d58b07b582941426ae6594
EvaYohane <79856216+AdamCSHU@users.noreply.github.com>
2023-03-16 03:00:25 +0000

    Added mode 1 and 2.

---
+++ Robotics_mode_1/src/Mode1.ino
@@ -0,0 +1,139 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+/*
+work on code for robotics mode 1 through weekend. Start mode 3 on saturday and mode 2 as well.
+//mode 1 is fully manual, make sure to slow down as proxmity nears person/survivor. 
+*/
+//proximity code inspired by https://github.com/pololu/zumo-32u4-arduino-library/blob/master/examples/SumoProximitySensors/SumoProximitySensors.ino
+
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4LineSensors lineSensors;
+Zumo32U4ProximitySensors proxSensors;
+
+uint16_t proxRightFrontActive, proxLeftFrontActive, proxLeftActive, proxRightActive;
+const int proxTolerance = 5;
+int cooldown = 0;
+
+char action;
+int speed = 100;
+
+unsigned int lineSensorValues[3];
+
+//mode 1.ino file, contains all code related to zumo mode 1 task including proximity which sends a message to the serial console if an object is detected on either side (left/right).
+void setup()
+{
+  Serial.begin(9600);
+  ledGreen(1);
+  proxSensors.initThreeSensors();
+}
+
+/*
+//code inspired by https://github.com/pvcraven/zumo_32u4_examples
+void printToSerial()
+{
+  static char buffer[80];
+  sprintf(buffer, "Person detected at co-ordinates: \n%d %d %d %d %d %d  %d %d %d  %4d %4d %4d %4d %4d\n",
+  proxSensors.countsLeftWithLeftLeds(),
+  proxSensors.countsLeftWithRightLeds(),
+  proxSensors.countsFrontWithLeftLeds(),
+  proxSensors.countsFrontWithRightLeds(),
+  proxSensors.countsRightWithLeftLeds(),
+  proxSensors.countsRightWithRightLeds(),
+proxLeftActive,
+proxRightActive,
+proxFrontActive
+  );
+  Serial.print(buffer);
+}
+*/
+
+void loop()
+{
+  if (Serial.available() > 0) 
+  { 
+          static uint16_t lastTime = 0;
+    if ((uint16_t)(millis()- lastTime) >= 100)
+    {
+
+      proxSensors.read();
+      
+        proxLeftFrontActive = proxSensors.countsFrontWithLeftLeds();
+        proxRightFrontActive = proxSensors.countsFrontWithRightLeds();
+        proxRightActive = proxSensors.countsRightWithRightLeds();
+        proxLeftActive = proxSensors.countsLeftWithLeftLeds();
+       
+      if (proxLeftActive >= proxTolerance && cooldown == 0){
+        Serial.print("person detected to the left");
+        delay(1000);
+        motors.setSpeeds(50, 50);
+        cooldown = 100;
+      } else if (proxRightActive >= proxTolerance && cooldown == 0){
+        Serial.print("person detected to the right");
+        delay(1000);
+        motors.setSpeeds(50, 50);
+        cooldown = 100;
+      }
+    }
+
+    action = Serial.read();  
+
+      switch (action)
+      {
+      case 'w':
+        // go forwards
+        ledYellow(1);
+        ledRed(0);
+        ledGreen(0);
+        motors.setSpeeds (speed, speed);
+        delay(100);
+        Serial.write("You are moving forwards");
+        break;
+      case 's':
+        // go backwards
+        ledYellow(0);
+        ledRed(0);
+        ledGreen(1);
+        motors.setSpeeds(speed*-1, speed*-1);
+        delay(100);
+        Serial.write("You are moving backwards");
+        break;  
+      case 'a':
+        // turn left
+        ledRed(1);
+        ledGreen(0);
+        ledYellow(0);
+        motors.setLeftSpeed(0);
+        motors.setRightSpeed(speed);
+        delay(100);
+        Serial.write("You are moving to the left");
+        break;   
+      case 'd':
+        // turn right
+        ledRed(1);
+        ledGreen(0);
+        ledYellow(0);
+        motors.setLeftSpeed(speed);
+        motors.setRightSpeed(0);
+        delay(100);
+        Serial.write("You are moving to the right");
+        break;      
+      default:
+        motors.setLeftSpeed(0);
+        motors.setRightSpeed(0);
+        ledYellow(0);
+        ledRed(0);
+        ledGreen(0);
+        delay(100);
+    }
+
+    delay(500);
+   }
+  
+}
+  
+void calibrateSensors(){
+
+}
+ 
---
+++ Robotics_mode_2/src/mode2.ino
@@ -0,0 +1,176 @@
+#include <Zumo32U4.h>
+
+Zumo32U4ButtonA buttonA;
+Zumo32U4Motors motors;
+Zumo32U4Buzzer buzzer;
+Zumo32U4LineSensors lineSensors;
+Zumo32U4ProximitySensors proxSensors;
+
+#define numSensors 5
+unsigned int lineSensorValues[numSensors];
+
+char action;
+uint16_t speed = 200;
+uint16_t max = 400;
+uint16_t min = 210;
+int cornerSpeed = 150;
+
+static bool corner = false;
+static bool rightObj = false;
+static bool leftObj = false;
+static bool Detected = false; 
+
+/*
+mode 2 for zumo robot, semi autonomous. Using line sensors along with proximity sensors to keep zumo inside of the lines, if a turn is detected, zumo will switch to manual controls for the user to make the turn, once the turn is complete
+zumo will continue in autonomous mode.
+*/
+void calibrateSensors()
+{
+  // Wait 1 second and then begin automatic sensor calibration
+  // by rotating in place to sweep the sensors over the line
+  delay(1000);
+  for(uint16_t i = 0; i < 120; i++)
+  {
+    if (i > 30 && i <= 90)
+    {
+      motors.setSpeeds(-200, 200);
+    }
+    else
+    {
+      motors.setSpeeds(200, -200);
+    }
+
+    lineSensors.calibrate();
+  }
+  motors.setSpeeds(0, 0);
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  lineSensors.initThreeSensors();
+  proxSensors.initThreeSensors();
+
+  calibrateSensors();
+
+}
+
+void mode3(){
+  motors.setRightSpeed(speed);
+  motors.setLeftSpeed(speed);
+
+  if (lineSensorValues[0] > max){
+    motors.setLeftSpeed(0);
+    motors.setRightSpeed(0);
+    delay(100);
+
+    motors.setLeftSpeed(speed);
+    motors.setRightSpeed(-speed);
+    delay(100);
+  }
+
+  if (lineSensorValues[2] > max){
+    motors.setLeftSpeed(0);
+    motors.setRightSpeed(0);
+    delay(100);
+
+    motors.setLeftSpeed(-speed);
+    motors.setRightSpeed(speed);
+    delay(100);
+  }
+}
+
+void mode1(){
+  motors.setSpeeds(0, 0);
+
+    if (Serial.available() > 0) 
+    { 
+
+    action = Serial.read();  
+
+      switch (action)
+      {
+      case 's' : case 'S':
+        // go backwards
+        ledYellow(0);
+        ledRed(0);
+        ledGreen(1);
+        motors.setSpeeds(speed*-1, speed*-1);
+        delay(100);
+        Serial.write("You are moving backwards");
+        break;  
+      case 'a' : case 'A':
+        // turn left
+        ledRed(1);
+        ledGreen(0);
+        ledYellow(0);
+        motors.setLeftSpeed(0);
+        motors.setRightSpeed(speed);
+        delay(100);
+        Serial.write("You are moving to the left");
+        break;   
+      case 'd': case 'D':
+        // turn right
+        ledRed(1);
+        ledGreen(0);
+        ledYellow(0);
+        motors.setLeftSpeed(speed);
+        motors.setRightSpeed(0);
+        delay(100);
+        Serial.write("You are moving to the right");
+        break;      
+    }
+    delay(500);
+   }
+}
+
+//autonomous code is inside the loop, autonomous mode will continue until zumo detects a turn, then manual controls will be given via the serial monitor or the associated gui, for the user to control zumo around the corner
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  proxSensors.read();
+
+  uint16_t leftSensor = proxSensors.countsLeftWithLeftLeds();
+  uint16_t rightSensor = proxSensors.countsRightWithRightLeds();
+  uint16_t leftCentreSensor = proxSensors.countsFrontWithLeftLeds();
+  uint16_t rightCentreSensor = proxSensors.countsFrontWithRightLeds();
+
+  uint16_t sampleTime = 0;
+  if ((uint16_t)(millis()) - sampleTime >=100){
+    sampleTime = millis();
+    lineSensors.read(lineSensorValues);
+  }
+
+  if ((lineSensorValues[0] > max && lineSensorValues[1] > cornerSpeed || lineSensorValues[2] > max && lineSensorValues[1] > cornerSpeed) || (leftSensor > 5 || rightSensor > 5 || (leftCentreSensor > 5 && rightCentreSensor > 5 && leftSensor > 5 && rightSensor > 5))){
+   
+    if (lineSensorValues[0] > max && lineSensorValues[1] > cornerSpeed || lineSensorValues[2] > max && lineSensorValues[1] > cornerSpeed)
+    {
+          Serial.print("corner detected \n");
+    }
+
+    if (leftSensor > 5 || rightSensor > 5 || (leftCentreSensor > 5 && rightCentreSensor > 5 && leftSensor > 5 && rightSensor > 5))
+    {
+      if (leftSensor > 5)
+      {
+          Serial.print("object detected to the left");
+        motors.setSpeeds(-speed, speed);
+      }
+      if (rightSensor > 5)
+      {
+          Serial.print("object detected to the left");
+        motors.setSpeeds(speed, -speed);
+      }
+      if (leftCentreSensor > 6 && rightCentreSensor > 6 && leftSensor > 5 && rightSensor > 5)
+      {
+          Serial.print("objected detected in front of zumo");
+        motors.setSpeeds(0, 0);
+      }
+      delay(200);
+    }
+    mode1();
+  }else{
+    mode3();
+  } 
+}
